4b5662f784ac81bb66673b879c1cb93092644890
Marcelo Silva <ribmarc@gmail.com>
2024-05-03 15:59:45 -0300

    feat: implement entity customer

--- src/Application/Ports/Primary/ICustomerService.ts
+++ src/Application/Ports/Primary/ICustomerService.ts
@@ -1,3 +1,7 @@
+import Customer from "../../Entities/Customer";
+
 export default interface ICustomerService {
-  
+  saveOrUpdate() : Promise<void>;
+  delete(): Promise<void>;
+  findByCpf(cpf: string): Promise<void>;
 }
