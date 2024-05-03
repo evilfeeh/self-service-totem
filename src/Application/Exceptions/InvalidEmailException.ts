b9d49907e05075587e92ebb1cc3a9844806571bc
Marcelo Silva <ribmarc@gmail.com>
2024-05-03 15:58:49 -0300

    feat: add exceptions

--- src/Application/Exceptions/InvalidEmailException.ts
+++ src/Application/Exceptions/InvalidEmailException.ts
@@ -1,3 +1,6 @@
-export default class InvalidEmailException {
-  
+export default class InvalidEmailException extends Error{
+  constructor(message: string = "Invalid email") {
+    super(message);
+    this.name = "InvalidEmailException";
+  }
 }
