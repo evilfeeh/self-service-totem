3a7ff126d94c50f443806cfb0501554629374a60
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-04 14:16:18 -0300

    fix: implement interfce methods to pass CIs

--- src/Adapters/Secondary/MySqlAdapter/Repositories/CustomerRepository.ts
+++ src/Adapters/Secondary/MySqlAdapter/Repositories/CustomerRepository.ts
@@ -1,5 +1,14 @@
+import Customer from "../../../../Application/Entities/Customer";
 import ICustomerRepository from "../../../../Application/Ports/Secondary/ICustomerRepository";
 
 export default class CustomerRepository implements ICustomerRepository {
-  
+  saveOrUpdate(customer: Customer): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  delete(cpf: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  findByCpf(cpf: string): Promise<Customer | undefined> {
+    throw new Error("Method not implemented.");
+  }
 }
