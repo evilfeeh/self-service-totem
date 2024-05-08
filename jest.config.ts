7d66b6d7c6110916eae069d7c144aa4e96339da8
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-05-07 23:58:38 -0300

    chore: add paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,21 @@
 // jest.config.js
 module.exports = {
-  clearMocks: true,
-  collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/Application/Entities/*.ts','<rootDir>/src/Application/ValueObjects/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text-summary', 'lcov'],
-  preset: 'ts-jest',  
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-};
+    clearMocks: true,
+    collectCoverage: false,
+    collectCoverageFrom: [
+        '<rootDir>/src/Application/Entities/*.ts',
+        '<rootDir>/src/Application/ValueObjects/*.ts',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text-summary', 'lcov'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
+    moduleNameMapper: {
+        '^@Application/(.*)$': '<rootDir>/src/Application/$1',
+        '^@Shared/(.*)$': '<rootDir>/src/Shared/$1',
+        '^@Entities/(.*)$': '<rootDir>/src/Application/domain/Entities/$1',
+        '^@Adapters/(.*)$': '<rootDir>/src/Adapters/$1',
+        '^@Exceptions/(.*)$': '<rootDir>/src/Application/domain/Exceptions/$1',
+    },
+}
