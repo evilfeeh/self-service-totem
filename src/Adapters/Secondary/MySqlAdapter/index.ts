fa658bb87f362f72a2acff839f5ecaff8c4e7579
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-10 21:46:31 -0300

    feat: estabilishing a connection with database

--- src/Adapters/Secondary/MySqlAdapter/index.ts
+++ src/Adapters/Secondary/MySqlAdapter/index.ts
@@ -1,16 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm";
-import { Customer } from "./models/Customer";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { Customer } from './models/Customer'
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'MfDZk"rTtn[f>j%',
-  database: 'self-attendence',
-  logging: false,
-  entities: [Customer],
+    type: 'mysql',
+    host: 'mysql',
+    port: 3306,
+    username: 'mcdonalds',
+    password: 'MfDZk"rTtn[f>j%',
+    database: 'self-attendence',
+    logging: false,
+    entities: [Customer],
 })
 
-AppDataSource.initialize().then(() => {}).catch((error) => console.log(error));
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Connection has been established successfully')
+    })
+    .catch((error) => console.log(error))
