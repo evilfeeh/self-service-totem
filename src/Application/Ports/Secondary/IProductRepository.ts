86e5b2482547cbc30414a06ad4d6c84eac3609aa
devtomasjr <dev@tomasjr.com>
2024-05-11 15:52:09 -0300

    feat: Add Product model and repository implementation

--- src/Application/Ports/Secondary/IProductRepository.ts
+++ src/Application/Ports/Secondary/IProductRepository.ts
@@ -4,6 +4,7 @@ import Product from '@Entities/Product'
 export default interface IProductRepository {
     create(product: Product): Promise<Either<Error, string>>
     update(product: Product): Promise<Either<Error, string>>
-    delete(id: number): Promise<Either<Error, string>>
+    delete(id: string): Promise<Either<Error, string>>
+    findById(id: Product['id']): Promise<Either<Error, Product>>
     findByCategory(category: Product['category']): Promise<Either<Error, Product[]>>
 }
