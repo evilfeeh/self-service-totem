07e4f0d1c1511c6badf56eb47453f5fca9e94c9d
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2024-05-11 15:21:30 -0300

    feat: implementing mercado pago interface

---
+++ src/Application/Ports/Secondary/QRCodeManager.ts
@@ -0,0 +1,39 @@
+export interface IQRCodeManager {
+    bearerToken: string
+    baseUrl: string
+    paymentStoreSponsorId: string
+    mercadoPagoUserId: string
+    mercadoPagoStoreId: string
+    mercadoPagoPosId: string
+    createPayment(): void
+    getPayment(): QRResponse
+    deletePayment(): void
+}
+
+export type QRResponse = BaseObj[]
+
+export interface BaseObj {
+    external_reference: string
+    total_amount: number
+    items: Item[]
+    title: string
+    description: string
+    sponsor: Sponsor
+    expiration_date: string
+    notification_url: string
+}
+
+export interface Item {
+    sku_number: string
+    category: string
+    title: string
+    description: string
+    unit_measure: string
+    quantity: number
+    unit_price: number
+    total_amount: number
+}
+
+export interface Sponsor {
+    id: number
+}
