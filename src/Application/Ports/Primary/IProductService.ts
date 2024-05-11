d096e0f89b3730a9cde21edd74fdcd54b4955517
devtomasjr <dev@tomasjr.com>
2024-05-11 15:52:48 -0300

    fix: Update ProductService to use ProductInputDTO and ProductOutputDTO

--- src/Application/Ports/Primary/IProductService.ts
+++ src/Application/Ports/Primary/IProductService.ts
@@ -1,12 +1,12 @@
 import { Either } from '@Shared/util/either'
-import Product from '@Entities/Product'
+import Product, { ProductInputDTO, ProductOutputDTO } from '@Entities/Product'
 
 export default interface IProductService {
     createProduct(
-        name: Product['name'],
-        category: Product['category'],
-        price: Product['price'],
-        description: Product['description'],
+        name: ProductInputDTO['name'],
+        category: ProductInputDTO['category'],
+        price: ProductInputDTO['price'],
+        description: ProductInputDTO['description'],
     ): Promise<Either<Error, string>>
     updateProduct(
         id: Product['id'],
@@ -16,5 +16,5 @@ export default interface IProductService {
         description: Product['description'],
     ): Promise<Either<Error, string>>
     deleteProduct(id: string): Promise<Either<Error, string>>
-    findByCategory(category: Product['category']): Promise<Either<Error, Product[]>>
+    findByCategory(category: Product['category']): Promise<Either<Error, ProductOutputDTO[]>>
 }
