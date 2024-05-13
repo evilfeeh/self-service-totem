efc97c3b42ff33effe6b31102b349c346b765126
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-13 18:43:25 -0300

    refactor: simplifying class to fake checkout

--- src/Adapters/Secondary/MercadoPagoAPI/QRCodeManager.ts
+++ src/Adapters/Secondary/MercadoPagoAPI/QRCodeManager.ts
@@ -1,65 +1,51 @@
 import {
     IQRCodeManager,
     QRResponse,
-} from '@Application/Ports/Secondary/IQRCodeManager'
-import { HttpRequest } from '../../../Shared/util/request'
+} from '../../../Application/Ports/Secondary/IQRCodeManager'
+import { Either, Right, Left } from '../../../Shared/util/either'
 
 export class QRCodeManager implements IQRCodeManager {
-    private readonly bearerToken: string | undefined
-    private readonly baseUrl: string | undefined
-    private readonly paymentStoreSponsorId: string | undefined
-    private readonly mercadoPagoUserId: string | undefined
-    private readonly mercadoPagoStoreId: string | undefined
-    private readonly mercadoPagoPosId: string | undefined
+    constructor() {}
 
-    constructor(private readonly httpRequest: HttpRequest) {
-        this.bearerToken = process.env.MERCADO_PAGO_BEARER_TOKEN
-        this.baseUrl = process.env.PAYMENT_BASE_URL
-        this.paymentStoreSponsorId = process.env.PAYMENT_STORE_SPONSOR_ID
-        this.mercadoPagoUserId = process.env.MERCADO_PAGO_USER_ID
-        this.mercadoPagoStoreId = process.env.MERCADO_PAGO_STORE_ID
-        this.mercadoPagoPosId = process.env.MERCADO_PAGO_POS_ID
+    async createPayment(amount: number): Promise<Either<Error, boolean>> {
+        try {
+            return Right(true)
+        } catch (error) {
+            return Left<Error>(error as Error)
+        }
     }
-
-    async createPayment(amount: number): Promise<boolean> {
-        this.httpRequest.put(
-            `${this.baseUrl}/instore/qr/seller/collectors/${this.mercadoPagoUserId}/stores/${this.mercadoPagoStoreId}/pos/${this.mercadoPagoPosId}/orders`,
+    async getPayment(): Promise<Either<Error, QRResponse>> {
+        return Right([
             {
-                'Content-Type': 'application/json',
-                Authorization: this.bearerToken,
-            },
-            {
-                cash_out: {
-                    amount,
-                },
-                description: 'Purchase description.',
-                external_reference: '12345',
+                external_reference: '1',
+                total_amount: 10,
                 items: [
                     {
-                        sku_number: 'A123K9191938',
-                        category: 'marketplace',
-                        title: 'Point Mini',
-                        description: 'This is the Point Mini',
-                        unit_price: 100,
+                        sku_number: '1',
+                        category: '1',
+                        title: 'Point',
+                        description: 'Point Mini',
+                        unit_measure: '1',
                         quantity: 1,
-                        unit_measure: 'unit',
-                        total_amount: 100,
+                        unit_price: 1,
+                        total_amount: 1,
                     },
                 ],
-                notification_url: 'http://www.yourserver.com/notification',
+                title: '1',
+                description: '1',
                 sponsor: {
-                    id: 662208785,
+                    id: 1,
                 },
-                title: 'Product order',
-                total_amount: 100,
-            }
-        )
-        return true
+                expiration_date: '1',
+                notification_url: '1',
+            },
+        ])
     }
-    getPayment(): Promise<QRResponse> {
-        throw new Error('Method not implemented.')
-    }
-    deletePayment(): Promise<boolean> {
-        throw new Error('Method not implemented.')
+    async deletePayment(): Promise<Either<Error, boolean>> {
+        try {
+            return Right(true)
+        } catch (error) {
+            return Left<Error>(error as Error)
+        }
     }
 }
