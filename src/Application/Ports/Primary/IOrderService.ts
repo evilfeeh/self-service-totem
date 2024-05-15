73b4ca25fe7b92504395c662c99102f9a8b64f1e
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-15 13:31:01 -0300

    refactor: changing some properties

--- src/Application/Ports/Primary/IOrderService.ts
+++ src/Application/Ports/Primary/IOrderService.ts
@@ -1,17 +1,19 @@
 import { Either } from '../../../Shared/util/either'
 import Order, { OrderOutputDTO } from '../../domain/Entities/Order'
+import Product from '../../domain/Entities/Product'
 
 export default interface IOrderService {
     createOrder(
         customer: OrderOutputDTO['customer'],
-        products: OrderOutputDTO['products'],
+        products: Product[],
         closed: OrderOutputDTO['closed']
     ): Promise<Either<Error, string>>
-    getOrder(id: Order['id']): Promise<Either<Error, OrderOutputDTO>>
-    getAllOrders(): Promise<Either<Error, OrderOutputDTO[]>>
+    getOrder(id: Order['id']): Promise<Either<Error, Order>>
+    getAllOrders(): Promise<Either<Error, Order[]>>
     updateOrder(
         id: Order['id'],
-        products: Order['products'],
-        closed: Order['closed']
+        customer: OrderOutputDTO['customer'],
+        products: Product[],
+        closed: OrderOutputDTO['closed']
     ): Promise<Either<Error, string>>
 }
