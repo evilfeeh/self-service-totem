ef412c58e360a798f32330f5c9f9b801ffd32854
devtomasjr <dev@tomasjr.com>
2024-05-18 12:06:15 -0300

    chore: Change orderId type to string in QRCodeManager

--- src/Adapters/Secondary/MercadoPagoAPI/QRCodeManager.ts
+++ src/Adapters/Secondary/MercadoPagoAPI/QRCodeManager.ts
@@ -5,22 +5,22 @@ import {
 import { Either, Right, Left } from '../../../Shared/util/either'
 
 export class QRCodeManager implements IQRCodeManager {
-    private externalApiDatabase: Map<number, any>
+    private externalApiDatabase: Map<string, any>
     private readonly baseIdQuantity: number = 50000
     constructor() {
         this.externalApiDatabase = new Map()
     }
 
-    async createPayment(payload: any): Promise<Either<Error, number>> {
+    async createPayment(payload: any): Promise<Either<Error, string>> {
         try {
-            const orderId = Math.floor(Math.random() * this.baseIdQuantity)
+            const orderId = payload.orderId
             this.externalApiDatabase.set(orderId, payload)
             return Right(orderId)
         } catch (error) {
             return Left<Error>(error as Error)
         }
     }
-    async getPayment(orderId: number): Promise<Either<Error, QRResponse>> {
+    async getPayment(orderId: string): Promise<Either<Error, QRResponse>> {
         try {
             const paymentOrder = this.externalApiDatabase.get(orderId)
             return Right(paymentOrder)
@@ -28,7 +28,7 @@ export class QRCodeManager implements IQRCodeManager {
             return Left<Error>(error as Error)
         }
     }
-    async deletePayment(orderId: number): Promise<Either<Error, boolean>> {
+    async deletePayment(orderId: string): Promise<Either<Error, boolean>> {
         try {
             this.externalApiDatabase.delete(orderId)
             return Right(true)
