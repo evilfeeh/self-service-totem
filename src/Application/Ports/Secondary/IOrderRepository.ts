e7c2c5494553bcf0033954db3ce05cb279a9e957
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-18 12:04:38 -0300

    refactor: update repository

--- src/Application/Ports/Secondary/IOrderRepository.ts
+++ src/Application/Ports/Secondary/IOrderRepository.ts
@@ -2,9 +2,9 @@ import { Either } from '../../../Shared/util/either'
 import Order from '../../../Application/domain/Entities/Order'
 
 export default interface IOrderRepository {
-    create(Order: Order): Promise<Either<Error, string>>
-    get(id: Order['id']): Promise<Either<Error, Order>>
+    create(order: Order): Promise<Either<Error, string>>
+    update(order: Order): Promise<Either<Error, string>>
+    delete(id: string): Promise<Either<Error, string>>
+    get(id: string): Promise<Either<Error, Order>>
     getAll(): Promise<Either<Error, Order[]>>
-    update(Order: Order): Promise<Either<Error, string>>
-    delete(cpf: string): Promise<Either<Error, string>>
 }
