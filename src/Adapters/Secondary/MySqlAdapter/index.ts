ed3493cf1227ec1e27e303b1edba0abf9f4e3505
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-20 14:59:00 -0300

    refactor: passing envs and forcing connection

--- src/Adapters/Secondary/MySqlAdapter/index.ts
+++ src/Adapters/Secondary/MySqlAdapter/index.ts
@@ -4,16 +4,18 @@ import { Customer } from './models/Customer'
 import { OrderItem } from './models/OrderItem'
 import { Order } from './models/Order'
 import { Product } from './models/Product'
+import { FirstStatement1716215951567 } from './migrations/1716215951567-FirstStatement'
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'mysql_self',
-    port: 3306,
-    username: 'mcdonalds',
-    password: 'MfDZk"rTtn[f>j%',
-    database: 'self-attendence',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     logging: false,
     entities: [Customer, Product, Order, OrderItem],
+    migrations: [FirstStatement1716215951567],
     synchronize: true,
 })
 
@@ -21,4 +23,6 @@ AppDataSource.initialize()
     .then(() => {
         console.log('Connection has been established successfully')
     })
-    .catch((error) => console.log(error))
+    .catch((error) => {
+        throw error
+    })
