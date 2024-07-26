7f03c7c181d36967bdcc7e43d84d38cac700d519
Marcelo Silva <ribmarc@gmail.com>
2024-07-26 17:33:21 -0300

    add gateway instance in routes

--- src_temp/External/Api/Routes/CustomerRoutes.ts
+++ src_temp/External/Api/Routes/CustomerRoutes.ts
@@ -3,20 +3,25 @@ import CustomerController from '../../../Controllers/CustomerController'
 import FindCustomerByCpfUseCase from '../../../UseCases/Customer/findByCpf/findCustomerByCpf.usecase'
 import RegisterCustomerUseCase from '../../../UseCases/Customer/register/register.usecase'
 import CustomerRepository from '../../Database/Repositories/DatabaseRepository/CustomerRepository'
+import CustomerGatewayRepository from '../../../Gateways/Customer/CustomerGatewayRepository'
 
 export default class CustomerRoutes {
     private readonly customerRepository: CustomerRepository
     private readonly customerController: CustomerController
     private readonly registerCustomerUseCase: RegisterCustomerUseCase
     private readonly findCustomerByCpf: FindCustomerByCpfUseCase
+    private readonly customerGatewayRepository: CustomerGatewayRepository
 
     constructor() {
         this.customerRepository = new CustomerRepository()
-        this.registerCustomerUseCase = new RegisterCustomerUseCase(
+        this.customerGatewayRepository = new CustomerGatewayRepository(
             this.customerRepository
         )
+        this.registerCustomerUseCase = new RegisterCustomerUseCase(
+            this.customerGatewayRepository
+        )
         this.findCustomerByCpf = new FindCustomerByCpfUseCase(
-            this.customerRepository
+            this.customerGatewayRepository
         )
 
         this.customerController = new CustomerController(
