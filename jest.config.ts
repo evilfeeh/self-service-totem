9a724f74ae7973aab1f22761cd595909413f29b6
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-08-20 22:12:14 -0300

    chore: update jest to get coverage from all src files

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,9 @@
 module.exports = {
     clearMocks: true,
     collectCoverage: false,
-    collectCoverageFrom: [
-        'src/Application/domain/Entities/*.ts',
-        'src/Application/domain/ValueObjects/*.ts',
-        'src/Application/services/*.ts',
-    ],
+    collectCoverageFrom: ['src/**/*.ts'],
     coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
+    coverageReporters: ['lcov'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
