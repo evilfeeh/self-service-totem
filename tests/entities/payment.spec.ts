6345f1498d4e3ee178807742f1790c3b620501a7
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-10-21 22:16:43 -0300

    feat: adding env to tests

--- tests/entities/payment.spec.ts
+++ tests/entities/payment.spec.ts
@@ -5,11 +5,15 @@ import { CategoryEnum } from '../../src/Entities/Enums/CategoryEnum'
 import Order from '../../src/Entities/Order'
 import Product from '../../src/Entities/Product'
 import OrderItem from '../../src/Entities/OrderItem'
-import { exec } from 'child_process'
+import dotenv from 'dotenv'
 
 describe('Payment entity', () => {
+    beforeAll(() => {
+        dotenv.config({
+            path: '.env.test',
+        })
+    })
     let payment: Payment
-    let order: Order
     beforeEach(() => {
         const quindim = new Product(
             '1',
@@ -56,5 +60,42 @@ describe('Payment entity', () => {
 
     it('should return status', () => {
         expect(payment.getStatus()).toBe(PaymentStatus.INITIALIZED)
+    })
+
+    it('should return id', () => {
+        expect(payment.getId()).toBe('1')
+    })
+
+    it('should return order id', () => {
+        expect(payment.getOrderId()).toBe('1')
+    })
+
+    it('should set value', () => {
+        payment.setValue(10.0)
+
+        expect(payment.getValue()).toBe(10.0)
+    })
+
+    it('should list products', () => {
+        const bridadeiro = new Product(
+            '2',
+            'Brigadeiro',
+            CategoryEnum.Dessert,
+            8.0,
+            'Uma sobremesa deliciosa com um toque brasileiro'
+        )
+
+        payment.setProducts([bridadeiro])
+
+        expect(payment.getProducts()).toBeInstanceOf(Array)
+        expect(payment.getProducts().length).toBe(1)
+    })
+
+    it('get payment url', () => {
+        expect(payment.getPaymentUrl()).toBe('http://localhost:3000/payments/1')
+    })
+
+    it('get notification url', () => {
+        expect(payment.getNotificationUrl()).toBe(process.env.NOTIFICATION_URL)
     })
 })
