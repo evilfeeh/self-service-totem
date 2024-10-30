40793bbe7d55dbb4ce44cde7be85b60262810cb6
Tomas Jr <dev@tomasjr.com>
2024-10-30 00:01:03 -0300

    test: add create product usecase test

--- tests/mocks/product.mock.ts
+++ tests/mocks/product.mock.ts
@@ -1,6 +1,16 @@
 import Product from "../../src/Entities/Product"
 import { CategoryEnum } from "../../src/Entities/Enums/CategoryEnum";
 import { randomUUID } from "crypto";
+import { InputCreateProductDTO } from "../../src/UseCases/Product/create/create.dto";
+
+export const createMockInputProduct = (): InputCreateProductDTO => {
+  return {
+    name: 'Hamburguer Classic',
+    category: CategoryEnum.Sandwich,
+    price: 10,
+    description: 'Muito suculento'
+  }
+}
 
 export const createMockProduct = (): Product => {
   return new Product(
